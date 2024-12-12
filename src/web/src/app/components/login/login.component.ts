605f6e15c8ffe1e10adbdf2f5ca95831a85bd530
thisismehdi <mehdifilalipro@gmail.com>
2024-12-12 14:36:34 +0100

    login

--- src/web/src/app/components/login/login.component.ts
+++ src/web/src/app/components/login/login.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../../service/auth.service";
+import { Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor(private authService: AuthService,private router: Router) {}
 
+  ngOnInit(): void {
+    this.login()
+  }
+
+  login() {
+    this.authService.login();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
 }
